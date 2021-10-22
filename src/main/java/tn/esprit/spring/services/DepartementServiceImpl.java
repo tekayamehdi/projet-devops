216424f88fb1304cbd93459c1c37234e90bea33a
tekayamehdi <mehdi.tekaya@esprit.tn>
2021-10-22 21:25:40 +0100

    update dependencies

--- src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
@@ -1,6 +1,8 @@
 package tn.esprit.spring.services;
 
-import java.util.List;
+import java.util.List; 
+import org.apache.log4j.Logger;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
